394a8b35be61cdd974ea63bb4a6b09fb17ed21e8
Max Winde <max.winde@gmail.com>
2012-08-10 18:38:26 +0200

    local article caching

--- app.js
+++ app.js
@@ -3,11 +3,38 @@ var RSS = require('rss');
 var http = require('http');
 var _ = require('underscore');
 var async = require('async');
+var mongoose = require('mongoose');
 
 var diffbotAPIKey = 'e6366ac27635f24e1f2ed901b950ed04';
 
+mongoose.connect('mongodb://localhost/better-pinboard-rss');
 app = express();
 app.listen(4243);
+
+app.configure(function() {
+    app.use(express.logger());
+});
+
+
+app.configure('development', function() {
+    app.use(express.errorHandler( { dumpExceptions: true, showStack: true} ));
+});
+
+var diffbotArticleShema = new mongoose.Schema({
+    url: { type: String, required: true, unique: true},
+    created: { type: Date, default: Date.now },
+    icon: String,
+    title: String,
+    html: String
+});
+
+diffbotArticleShema.statics.findByURL = function(url, next) {
+    this.model('DiffbotArticle').findOne({ url: url }, function(err, article) {
+        next(err, article);
+    });
+}
+
+var DiffbotArticle = mongoose.model('DiffbotArticle', diffbotArticleShema);
 
 app.get('/rss/secret::secret/u::username/network/', function (req, res) {
     var url = 'http://feeds.pinboard.in/json/secret:' + req.params.secret + '/u:' + req.params.username + '/network/';
@@ -31,33 +58,52 @@ app.get('/rss/secret::secret/u::username/network/', function (req, res) {
                var articles = JSON.parse(body);
 
                async.forEach(articles, function(article, next) {
-                   var url = 'http://www.diffbot.com/api/article' +
-                       '?token=' + diffbotAPIKey +
-                       '&url=' + encodeURIComponent(article.u) +
-                       '&html=true';
+                    DiffbotArticle.findByURL(article.u, function(err, savedArticle) {
+                        if (savedArticle) {
+                            console.log('article from cache');
+                            article.full = savedArticle;
+                            next();
+                        } else {
+                            console.log('loading article');
+                            var url = 'http://www.diffbot.com/api/article' +
+                                '?token=' + diffbotAPIKey +
+                                '&url=' + encodeURIComponent(article.u) +
+                                '&html=true';
 
-                   http.get(url, function(diffbotRes) {
-                       var body = '';
+                            http.get(url, function(diffbotRes) {
+                                var body = '';
 
-                       if (diffbotRes.statusCode != 200) {
-                           console.dir(diffbotRes);
-                           next();
-                           return;
-                       }
+                                if (diffbotRes.statusCode != 200) {
+                                    console.dir(diffbotRes);
+                                    next();
+                                    return;
+                                }
 
-                       diffbotRes.on('data', function(chunk) {
-                           body += chunk;
-                       });
+                                diffbotRes.on('data', function(chunk) {
+                                    body += chunk;
+                                });
 
-                       diffbotRes.on('end', function() {
-                           console.log('response from diffbot');
-                           var diffbotArticle = JSON.parse(body);
+                                diffbotRes.on('end', function() {
+                                    console.log('response from diffbot');
+                                    var diffbotArticle = JSON.parse(body);
 
-                           article.full = diffbotArticle;
+                                    article.full = diffbotArticle;
 
-                           next();
-                       });
-                   });
+                                    next();
+
+                                    var savedArticle = new DiffbotArticle({
+                                        url: diffbotArticle.url,
+                                        icon: diffbotArticle.icon,
+                                        title: diffbotArticle.title,
+                                        html: diffbotArticle.html
+                                    });
+                                    savedArticle.save();
+                                });
+                            });
+                        }
+                    });
+
+
                }, function(err) {
 
                    var feed = new RSS({
